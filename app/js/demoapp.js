57bef0814777d6332da360e842478cbb15530f4b
mtamer <mark.tamer01@gmail.com>
2014-03-28 21:48:14 -0400

    Got first book to print out

--- app/js/demoapp.js
+++ app/js/demoapp.js
@@ -1,22 +1,30 @@
 $(document).foundation();
-$(document).ready(function () {
-  $.getJSON("/api/books/1", function (book) {
-      window.console.log(book);
-      $demoapp = $('.demoapp');
-      $demoapp.append("<h1>"+book.meta.title[0]+"</h1>");
-      $demoapp.append("<h5>"+book.meta.creator[0]+"</h5>");
-      $demoapp.append("<p>"+book.data.chapter[1].title+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[1]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[2]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[3]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[4]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[5]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[7]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[8]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[9]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[10]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[11]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[12]+"</p>");
-      $demoapp.append("<p>"+book.data.chapter[1].text[13]+"</p>");
-    });
+$(document).ready(function() {
+	booker(1);
 });
+
+var booker = function (id) {
+	$.getJSON("/api/books/"+id, function (book) {
+		window.console.log(book.data.chapter);
+		$demoapp = $('.demoapp');
+		$demoapp.append("<h1>"+book.meta.title+"</h1>");
+		$demoapp.append("<h5>"+book.meta.creator+"</h5>");
+		for (var p in book.data.part) {
+			for (var chap in book.data.part[p].chapter) {
+				window.console.log("hello");
+				$demoapp.append("<p>"+book.data.part[p].chapter[chap].title+"</p>");
+				for (var par in book.data.part[p].chapter[chap].text) {
+					$demoapp.append("<p>"+book.data.part[p].chapter[chap].text[par]+"</p>");
+				};
+			};
+		}
+		for (var chap in book.data.chapter) {
+			window.console.log("hello");
+			$demoapp.append("<p>"+book.data.chapter[chap].title+"</p>");
+			for (var par in book.data.chapter[chap].text) {
+				$demoapp.append("<p>"+book.data.chapter[chap].text[par]+"</p>");
+			};
+		};
+	});
+
+};
