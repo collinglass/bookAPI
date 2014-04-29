aa739f67cd269bb595591be6eb055bdce4822d95
Collin Glass <collinglass@gmx.com>
2014-04-28 20:34:43 -0400

    Change name

---
+++ bookAPI.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/collinglass/bookAPI/server/ctrl"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.Println("Starting Server")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/books/", ctrl.GetBookList()).Methods("GET")
+	r.HandleFunc("/api/books/{id:[0-9]+}", ctrl.GetBook()).Methods("GET")
+
+	http.Handle("/api/", r)
+	http.Handle("/", logHandler(http.FileServer(http.Dir("./app/"))))
+
+	log.Println("Listening...")
+	panic(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+}
+
+func logHandler(h http.Handler) http.Handler {
+	return handlers.LoggingHandler(os.Stdout, h)
+}
